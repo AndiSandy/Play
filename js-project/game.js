c4392ad5e2c0ef5a3ac13bfe747077a0e630e567
magic0115 <magic0115@163.com>
2016-05-24 15:53:43 +0800

    suning2016年5月24日15:53:39

--- js-project/game.js
+++ js-project/game.js
@@ -16,74 +16,79 @@ var Game = Class.extend({
 	//发生倍数概率
 	rate : { '0' : 21.46, '0.5' : 39.73, '1': 23.71, '3':10, '5' : 5.12},
 	rateArr : [],
+	seedArr :[],
 	count : 0,
 	radix : 10000,
 	initNum : 0,
 	progress : '0',
 	init : function(){
-		this.rateArr.length = this.radix;
-	},
-	init1 : function(){
 		var total = 0;
-		var initNum = 0;
 		for( var multiple in this.rate ){
 			var brate = this.rate[multiple];
 			var rate =  Math.ceil ( brate * ( this.radix / 100 ) );
 			total += rate;
 		}
-		//= this.radix
+		this.total = total;
 		this.rateArr.length = total;
+		this.seedArr.length = total;
+		for(var i = 0; i < total; i ++){
+			this.seedArr[i] = i;
+		}
+	},
+	init1 : function(){
+		var total = this.total;
+		var initNum = 0;
 		console.info("要初始化%s个数据",total);
 		for( var multiple in this.rate ){
+			this.thread(multiple);
+			//break;
+		}
+		console.info('初始化完毕,总共初始化%s个数据',total);
+	},
+	thread :function(multiple){
+		var scope = this;
+		function task(multiple){
 			var brate = this.rate[multiple];
 			var rate =  Math.ceil ( brate * ( this.radix / 100 ) );
 			console.info("正在初始化",brate,rate,multiple);
 			//this.thread(rate,multiple);
 			/**/
 			for(var i = 0; i < rate; ){
-				var at = this.random();
-				var miss = 0;
-				while( this.rateArr[at||0] != null ){
-					miss ++;
-					if( miss > 10 ){
-						at = this.selectUninit();
-					}else{
-						at = this.random();
-					}
-				}
+				var at = this.random4seed();
 				//console.info(multiple,at);
 				this.rateArr[at] = multiple;
 				i ++;
-				this.initNum = initNum ++;
-				this.progress = Math.round((initNum/this.radix)*10000)/100;
+				this.initNum ++;
+				this.progress = Math.round((this.initNum/this.radix)*10000)/100;
 				//console.info("%s-初始化进度%s%",initNum,this.progress);
+				//this.$scope.$apply();
 			}
-			//break;
-		}
-		console.info('初始化完毕,总共初始化%s个数据',total);
-	},
-	selectUninit : function(){
-		var uninit = [];
-		for(var i = 0; i < this.rateArr.length; i ++){
-			if( this.rateArr[i] == null ){
-				uninit.push(i);
-			}
-		}
-		var at = this.seed(uninit.length);
-		var atValue = uninit[at];
-		assertNotNull(atValue,"selectUninit error ：" + at);
-		return atValue ;
+			this.$scope.$apply();
+		};
+		task.apply(scope,[multiple]);
+		/*
+		setTimeout(function(){
+			task.apply(scope,[multiple]);	
+		},1000);*/
 	},
 	seed : function( seed ){
 		return parseInt( (seed || this.radix) * Math.random() );
 	},
-	random : function(){
-		var r = (new Date().getTime() + (++this.count) * this.seed() ) % this.radix ;
+	random4seed : function(){
+		var seedNum = this.seedArr.length;
+		var seedAt = this.seed(seedNum);
+		var	rateAt = this.seedArr[seedAt];
+		//remove seedAt
+		this.seedArr.splice(seedAt,1);
+		return rateAt;
+	},
+	random : function(seed){
+		var r = (new Date().getTime() + (++this.count) * this.seed(seed) ) % (seed || this.radix) ;
 		assertNotNull(r,"random error");
 		return r;
 	},
 	random4rate : function(){
-		var r = this.random();
+		var r = this.random(this.total);
 		return {rate:this.rateArr[r] || this.rateArr[this.count],at:r};
 	},
 	play : function( yund ){
@@ -94,20 +99,25 @@ var Game = Class.extend({
 			console.info("%c恭喜您,云钻x%s倍,获得了%s个云钻!",_3dstyle,rate,y);
 		}
 		var o = {rate : rate, yund : y,at:rateo.at };
+		hover(o);
 		console.info("您获得x%s倍%s云钻,概率位%s",o.rate,o.yund,o.at);
 		return o;
 	}
 });
 app.controller('playCtrl', function($scope,$timeout) {
-	var s = new Date().getTime();
+	var timeid = "初始化对象";
+		console.time(timeid);
 	var game = window.game = new Game()
-	var e = new Date().getTime();
-	console.info("耗时",(e-s))
+	console.timeEnd(timeid);
+	game.$scope = $scope;
 	$scope.yund = 10;
 	$scope.game = game;
 	$scope.cssMap = cssMap;
 	$scope.title = "play game now";
 	$timeout(function(){
+		var timeid = "初始化数据";
+		console.time(timeid);
 		game.init1();
+		console.timeEnd(timeid);
 	},2000);
 });
